22db5e868a6a7571dedcacda86164c459e6ef149
DaniloEspinosa <danilopespinosa@gmail.com>
2023-12-04 11:43:05 +0100

    Añadiendo fichero 02_operadores.py

---
+++ 02_operadores.py
@@ -0,0 +1,12 @@
+# Operadores
+
+num1 = 10
+num2 = 2
+num3 = 11
+
+print("La suma es", num1 + num2)
+print("La resta es", num1 - num2)
+print("La multiplicación es", num1 * num2)
+print("La división es", num1 / num2)
+print("El módulo es", num3 % num2)
+print("El exponente es", num1 ** num2)
