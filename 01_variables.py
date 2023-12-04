f0bf23a6815e7562122a35c7188be34bd99b45fd
DaniloEspinosa <danilopespinosa@gmail.com>
2023-12-04 10:53:43 +0100

    Primer commit con las variables primitivas de python

---
+++ 01_variables.py
@@ -0,0 +1,11 @@
+# Crear una variable en python
+
+nombre = "Danilo"   # Variable tipo string
+numero1 = 7         # Variable tipo número
+numero2 = 10.5      # Variable tipo decimal
+booleano1 = True    # Variable tipo booleano
+booleano2 = False   # Variable tipo booleano
+
+# print es una función que muestra información por el terminal
+print(nombre)
+print(type(numero2))
